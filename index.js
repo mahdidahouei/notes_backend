bc014a1797aa9c690b42298cb058c7107abb66c9
ramzorazcafe <166458248+ramzorazcafe@users.noreply.github.com>
2024-07-19 22:41:48 +0330

    added cors

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const connectDB = require('./src/db');
 const { specs, swaggerUi } = require('./swagger');
 const { exec } = require('child_process'); // Import exec
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -16,6 +17,7 @@ connectDB();
 
 // Middleware
 app.use(express.json());
+app.use(cors());
 
 // Redirect root to /api-docs
 app.get('/', (req, res) => {
